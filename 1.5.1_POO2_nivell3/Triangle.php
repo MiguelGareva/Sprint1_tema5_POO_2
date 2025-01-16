f42f433486daec5193e74aa01522c729aedf91a4
miguel garcia <miguel.gareva@gmail.com>
2025-01-16 10:39:13 +0100

    previous exercise files copied

---
+++ 1.5.1_POO2_nivell3/Triangle.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+require_once "Shape.php";
+
+
+class Triangle extends Shape{
+
+    public function calcularArea(): float{
+        return ($this->base * $this->altura) / 2;
+    }
+}
+?>
